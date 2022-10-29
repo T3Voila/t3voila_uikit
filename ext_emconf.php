1d6e77a035c94e3389d792d40d129c433bfa585a
Alexander Opitz <opitz.alexander@googlemail.com>
2022-10-29 12:23:34 +0200

    [TASK] Update version dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,16 +3,16 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'UIkit theme for TV+',
     'description' => 'Theme for TemplaVoilà! Plus using fluid',
     'author' => 'Alexander Opitz',
-    'author_email' => 'opitz@extrameile-gehen.de',
-    'author_company' => 'Extrameile GmbH',
+    'author_email' => 'alexander.opitz@davitec.de',
+    'author_company' => 'Davitec GmbH',
     'version' => '0.1.0',
     'state' => 'alpha',
     'clearcacheonload' => 1,
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-11.5.99',
-            'templavoilaplus' => '8.0.4-',
-            'tvplus_fluid' => '0.0.1-',
+            'templavoilaplus' => '8.1.2-',
+            'tvplus_fluid' => '0.3.0-',
         ],
     ],
 ];
