9b220df391b924791a3e8be55bf6b419c2732527
Alexander Opitz <opitz.alexander@googlemail.com>
2023-03-27 11:19:54 +0200

    [RELEASE] Release 0.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,13 +5,13 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Alexander Opitz',
     'author_email' => 'alexander.opitz@davitec.de',
     'author_company' => 'Davitec GmbH',
-    'version' => '0.1.0',
+    'version' => '0.2.0',
     'state' => 'alpha',
     'clearcacheonload' => 1,
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99',
-            'templavoilaplus' => '8.1.2-',
+            'typo3' => '11.5.0-11.5.99',
+            'templavoilaplus' => '8.1.3-8.2.99',
             'tvplus_fluid' => '0.3.0-',
         ],
     ],
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or die();
 
 // @TODO This line can be removed after cache is implemented
 \Tvp\TemplaVoilaPlus\Utility\ExtensionUtility::registerExtension('t3voila_uikit');
