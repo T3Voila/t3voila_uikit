fc49904a9d49d6948d42c915feaeb6912e36ff5b
Alexander Opitz <opitz.alexander@googlemail.com>
2024-02-28 00:35:41 +0100

    Update to TYPO3v12 as minimum with TV+ 12.0.0 as minimum

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,13 +5,13 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Alexander Opitz',
     'author_email' => 'alexander.opitz@googlemail.com',
     'author_company' => 'IBC Solar AG',
-    'version' => '0.3.0',
+    'version' => '0.4.0-dev',
     'state' => 'alpha',
     'clearcacheonload' => 1,
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-12.4.99',
-            'templavoilaplus' => '11.0.0-11.0.99',
+            'templavoilaplus' => '12.0.0-12.0.99',
         ],
     ],
 ];
